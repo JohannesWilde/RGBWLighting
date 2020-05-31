d3d477867818e3277e2128e8b78b59d22f7dfa07
Johannes Wilde <git@jowil.de>
2020-05-31 14:30:29 +0200

    Remove unnecessary comments.

--- src/main.ino
+++ src/main.ino
@@ -29,14 +29,6 @@ static int16_t DebugOut(char ch) { if (ch == (char)'\n') Serial.println(""); els
 
 // Declare our NeoPixel strip object:
 static Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
 void setup()
 {
